c9e507a23f067db95039ec996456bd22302c5742
QSP <QSP@QSP>
2022-06-13 17:09:11 +0530

    HiHiii

---
+++ src/test/java/testscripts/Test5.java
@@ -0,0 +1,12 @@
+package testscripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test5 {
+	@Test
+	public void tc1() {
+	Reporter.log("hello appium",true);	
+	}
+
+}
